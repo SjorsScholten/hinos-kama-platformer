362dc15f8afbcea3f81cd0be0e7991d0965b2b45
SjorsScholten <scholtensjors22@outlook.com>
2022-12-10 22:17:45 +0100

    blade state and camera behaviour

--- code/Blade.cs
+++ code/Blade.cs
@@ -1,10 +1,95 @@
-public class Blade : MonoBehaviour {
+public class BladeController : MonoBehaviour, IStateMachine<BladeController> {
+    private SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D body;
+    private Vector2 velocity;
+
+    private Vector2 throwingDirection;
+    private bool thrown;
+
+    private void FixedUpdate() {
+        velocity = body.velocity;
+
+        if(thrown) {
+            velocity = throwingDirection
+        }
+
+        body.velocity = velocity
+    }
 
     private void OnCollisionEnter(Collision collision) {
 
     }
+
+    private void Move() {
+
+    }
+
+    public void ThrowTowardsDirection(Vector2 direction) {
+        thrown = true;
+        throwingDirection = direction;
+    }
+
+    public void Enable() {
+        spriteRenderer.enabled = true;
+        body.simulated = true;
+    }
+
+    public void Disable() {
+        spriteRenderer.enabled = false;
+        body.simulated = false;
+    }
 }
 
-public class AnchorPoint : MonoBehaviour {
+public class BladeState : State<BladeController> {
+    protected BladeStateFactory factory;
 
+    public BladeState(BladeController source, string name, BladeStateFactory factory) : base(source, name) {
+        this.factory = factory;
+    }
+}
+
+public class BladeStateFactory {
+    private readonly IdleBladeState idelState;
+    private readonly ThrownBladeState thrownState;
+
+    public BladeStateFactory(BladeController source) {
+
+    }
+
+    public IdleBladeState GetIdleState() => idelState;
+
+    public ThrownBladeState GetThrownState() => thrownState;
+}
+
+public class IdleBladeState : BladeState {
+
+    public override void Enter() {
+
+    }
+
+    public override void Exit() {
+
+    }
+
+    public override void Update() {
+        if(thrown) {
+            SwitchState(factory.GetThrownState());
+        }
+    }
+}
+
+public class ThrownBladeState : BladeState {
+
+    public override void Enter() {
+        Enable();
+    }
+
+    public override void Exit() {
+
+    }
+
+    public override void Update() {
+
+    }
 }
---
+++ docs/artificialIntelligence/A02_camera.md
@@ -0,0 +1,8 @@
+# Camera
+
+- The player has a v-cam
+- The blade has a v-cam
+- There is a v-cam for when the player is targeting. The camera zooms out to show the space where the blade will move towards
+- There is a target group for when the player is anchored to a target
+
+? Does each enemy need a v-cam for when he performs an important action
